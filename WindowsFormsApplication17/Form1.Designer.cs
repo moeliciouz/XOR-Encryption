e798ce7f6c06581498028e7850541a5e96270201
MoeliciouZ <moritz.b.hoffmann@gmail.com>
2017-05-20 12:58:11 +0200

    Add files via upload
    its the base of the app, the encrption already works but i have some probs while decipher (seems like the letters have an unwanted shift of one)

---
+++ WindowsFormsApplication17/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace XOR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(21, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(301, 247);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(378, 24);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(317, 247);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(258, 377);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(176, 32);
+            this.richTextBox3.TabIndex = 2;
+            this.richTextBox3.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 426);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Verschlüsseln";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(401, 426);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 31);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Entschlüsseln";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(316, 361);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Passphrase";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 297);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Klartext";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(523, 318);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Chiffrat";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 588);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "XOR Cipher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void button2_Click()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApplication17/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace XOR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Verschlüsselung auf Button 1
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Strings deklarieren
+            string klarText = richTextBox1.Text;
+            string Passwort = richTextBox3.Text;
+            string Verschlüsselung = "";
+
+            
+            // Ascii zu Bit Array
+
+            byte[] klarTextArray = System.Text.Encoding.ASCII.GetBytes(klarText);
+            byte[] PasswortTextArray = System.Text.Encoding.ASCII.GetBytes(Passwort);
+                                   
+            // klarText in Binär
+
+            string klarTextBinaer = string.Empty;
+
+            // Für jeden Buchstaben des klarText konvertiere Ascii in Binär (8 Bit Blöcke)
+
+            foreach (char KlarTextBuchstabe in klarText)
+            {
+                klarTextBinaer += Convert.ToString((int)KlarTextBuchstabe, 2).PadLeft(8, '0');
+            }
+
+            // Passwort in binär
+
+            string PasswortBinaer = string.Empty;
+
+            // Für jeden Buchstaben des Passwort konvertiere Ascii in Binär (8 Bit Blöcke)
+
+            foreach (char PasswortBuchstabe in Passwort)
+            {
+                PasswortBinaer += Convert.ToString((int)PasswortBuchstabe, 2).PadLeft(8, '0');
+            }
+            
+            // Deklariere einen neuen Index und zähle mit dem das Passwort auf die Länge des klarText hoch
+
+            int j = 0;
+
+            // Verschlüsselung XOR (mit xor verschlüssel jedes Bit des klarText mit jedem Bit des Passworts, erweitere das Passwort auf jeden Buchstaben (Binär) im Text)
+
+            for (int i = 0; i < klarTextBinaer.Length; i++)
+            {
+                
+                // Verschlüsselung
+
+                int AsciiKlarTextIndex = Convert.ToInt32(klarTextBinaer[i]);
+                int AsciiPasswortIndex = Convert.ToInt32(PasswortBinaer[j]);
+
+                // Passwort erweitern
+
+                j = j + 1 == PasswortBinaer.Length ? 0 : j + 1;
+
+                int SchluesselString = AsciiKlarTextIndex ^= AsciiPasswortIndex;
+
+                Verschlüsselung += SchluesselString;
+
+                // Ausgabe
+
+                richTextBox2.Text = Verschlüsselung;
+            }
+        }
+
+        // Entschlüsselung auf Button 1
+         
+        private void button2_Click(object sender, EventArgs e)
+
+        {
+            //Strings deklarieren
+
+            string Cipher = richTextBox2.Text;
+            string Passwort = richTextBox3.Text;
+
+            string ReverseStringString = "";
+
+            // Passwort Array
+
+            byte[] PasswortTextArray = System.Text.Encoding.ASCII.GetBytes(Passwort);
+
+            // Passwort in Binär in 8 Bit Blöcke
+
+            string PasswortBinaer = string.Empty;
+            foreach (char PasswortBuchstabe in Passwort)
+            {
+                PasswortBinaer += Convert.ToString((int)PasswortBuchstabe, 2).PadLeft(8, '0');
+            }
+            int j = 0;
+
+            // Entschlüsselung -> xor cipher mit Passwort und erhalte klarText
+             
+            for (int i = 0; i < Cipher.Length; i++)
+
+            {
+
+                int CipherIndex = Convert.ToInt32(Cipher[i]);
+                int AsciiPasswortIndex = Convert.ToInt32(PasswortBinaer[j]);
+
+                j = j + 1 == PasswortBinaer.Length ? 0 : j + 1;
+
+                int ReverseString = CipherIndex ^= AsciiPasswortIndex;
+
+                ReverseStringString += ReverseString;
+
+                // Ausgabe
+
+                richTextBox1.Text = ReverseStringString;
+            }
+
+            richTextBox1.Text = aufteilen(ReverseStringString);
+        }
+
+        // Teile die Bit des neuen Schlüsselstring auf
+         
+        public string aufteilen(string ReverseStringString) 
+        {
+            string cipher = richTextBox2.Text;
+            string ziffer = "";
+            int[] array = new int[8];
+            int z = 0;
+
+            for (int p = 0; p < ReverseStringString.Length; p++)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    array[i] = ReverseStringString[z % ReverseStringString.Length] - 48;
+
+                    z++;
+
+                    richTextBox1.Text = ziffer;
+                }
+
+                ziffer += umwandel(array);
+                
+            }
+
+            return ziffer;
+        }
+
+        // Wandel die Bit Blöcke des entschlüsselten und aufgeteilten String wieder in Buchstaben um 
+
+        public char umwandel(int[] array)
+        {
+            int zahl = 0;
+            int g = 7;
+            char ziffer;
+
+            for (int c = 0; c < array.Length; c++)
+            {
+                if (array[c] == 1)
+                    array[c] += 1;
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                zahl += (int)Math.Pow(array[i], g);
+                g--;                             
+            }            
+
+            ziffer = (char)zahl;
+            return ziffer;
+        }        
+    }
+}
